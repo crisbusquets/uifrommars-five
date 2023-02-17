9c15b3dd1351aa64bc84806f996d30049c47a1d5
Cris Busquets <ileo.red@gmail.com>
2023-02-17 17:27:02 +0100

    Hide newsletter widget from pages
    [x] The mailerlite widget is hidden in:
    
    /gracias/ (ID 18)
    /aviso-legal/ (182)
    /contacto/ (ID 152)
    /gracias-correo-al-dia/ (ID 4677)
    /gracias-correo-portfolio/ (ID 4692)
    /gracias-correo-proceso/ (ID 4699)
    /gracias-correo-cambio/ (ID 4701)
    /gracias-correo-datos/ (4690)
    /politica-de-privacidad/ (405)

--- page.php
+++ page.php
@@ -8,7 +8,12 @@
             <h1><?php the_title(); ?></h1>
             <?php the_content(); ?>
 
+
+            <!-- les ID son de producció, les de local son 229, 182, 152, 405 -->
+            <?php if( ! is_page(array(18, 182, 152, 4677, 4692, 4699, 4701, 4690, 405)) ) : ?>
+
             <hr />
+
             <div class="newsletter-post">
                 <h3>Subscríbete a uiFromMars</h3>
                 <p>Un lugar construido exclusivamente para diseñador@s UI/UX y de producto que buscan crecer día tras
@@ -16,7 +21,16 @@
                 <p>¡Únete a <mark><?php echo do_shortcode( '[mailerlite-subscribers]' ); ?></mark> diseñador@s y
                     empieza ya!</p>
                 <?php get_template_part( 'mailerlite', 'widget' ); ?>
+
             </div>
+
+            <?php else: ?>
+
+            &nbsp;
+
+            <?php endif;?>
+
+
         </article>
         <?php endwhile; else: ?>
 
