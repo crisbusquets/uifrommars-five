0806578d6baff8bfe7d9fefc126f2981cbbf96ec
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2024-03-24 02:15:30 +0100

    Remove blank lines (author.php)

--- author.php
+++ author.php
@@ -9,10 +9,8 @@ get_header(); ?>
 // Set the Current Author Variable $curauth
 $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
 ?>
-
 <main id="page">
     <div class="wrapper">
-
         <article class="page">
             <div class="author-heading">
                 <div class="author-photo">
@@ -26,38 +24,30 @@ $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : g
             <h2>Sobre mí</h2>
             <?php echo $curauth->user_description; ?>
             </p>
-
             <hr />
         </article>
     </div>
 </main>
-
 <aside>
     <div class="wrapper">
         <h2 class="author-related-posts">Últimos artículos escritos por
             <?php echo $curauth->nickname; ?></h2>
-        <p class="author-related-posts">Estos son los últimos nueve artículos que he escrito en uiFromMars (de un total
-            de
+        <p class="author-related-posts">Estos son los últimos nueve artículos que he escrito en uiFromMars (de un total de
             <?php echo '' . count_user_posts( $q->ID ); ?>).</p>
         <div class="content-grid">
-
             <?php
-// Define our WP Query Parameters
-$the_query = new WP_Query( 'posts_per_page=9' ); ?>
-
+            // Define our WP Query Parameters
+            $the_query = new WP_Query( 'posts_per_page=9' ); ?>
             <?php
-// Start our WP Query
-while ($the_query -> have_posts()) : $the_query -> the_post();
-// Display the Post Title with Hyperlink
-?>
-
+            // Start our WP Query
+            while ($the_query -> have_posts()) : $the_query -> the_post();
+            // Display the Post Title with Hyperlink
+            ?>
             <article>
                 <a href="<?php the_permalink(); ?>" class="category-image">
                     <?php the_post_thumbnail( 'blog-thumbnails' ); ?>
                 </a>
-
                 <?php the_category(' ') ?>
-
                 <h3>
                     <a href="<?php the_permalink(); ?>">
                         <?php the_title(); ?>
@@ -65,14 +55,12 @@ while ($the_query -> have_posts()) : $the_query -> the_post();
                 </h3>
             </article>
             <?php
-// Repeat the process and reset once it hits the limit
-endwhile;
-wp_reset_postdata();
-?>
-
+            // Repeat the process and reset once it hits the limit
+            endwhile;
+            wp_reset_postdata();
+            ?>
         </div>
     </div>
 </aside>
-
 
 <?php get_footer(); ?>
