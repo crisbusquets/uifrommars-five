ccb04a8e8dc191e3cb684e1725c1c6f284382ee9
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2024-03-25 15:51:48 +0100

    Format page.php

--- page.php
+++ page.php
@@ -1,45 +1,32 @@
 <?php get_header(); ?>
 
 <main id="page">
-    <div class="wrapper">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
+  <div class="wrapper">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <article class="content-narrow">
-            <h1><?php the_title(); ?></h1>
-            <?php the_content(); ?>
-
-
-            <!-- les ID son de producció, les de local son 229, 182, 152, 405 -->
-            <?php if( ! is_page(array(18, 182, 152, 4677, 4692, 4699, 4701, 4690, 405)) ) : ?>
-
+          <h1><?php the_title(); ?></h1>
+          <?php the_content(); ?>
+          <?php
+          // The following array of ids are from production. The ids for local are: 229, 182, 152, 405
+          if (!is_page(array(18, 182, 152, 4677, 4692, 4699, 4701, 4690, 405))) : ?>
             <hr />
-
             <div class="newsletter-post">
-                <h3>Subscríbete a uiFromMars</h3>
-                <p>Un lugar construido exclusivamente para diseñador@s UI/UX y de producto que buscan crecer día tras
-                    día :)</p>
-                <p>¡Únete a <mark><?php echo do_shortcode( '[mailerlite-subscribers]' ); ?></mark> diseñador@s y
-                    empieza ya!</p>
-                <?php get_template_part( 'mailerlite', 'widget' ); ?>
-
+              <h3>Subscríbete a uiFromMars</h3>
+              <p>Un lugar construido exclusivamente para diseñador@s UI/UX y de producto que buscan crecer día tras día :)</p>
+              <p>¡Únete a <mark><?php echo do_shortcode('[mailerlite-subscribers]'); ?></mark> diseñador@s y empieza ya!</p>
+              <?php get_template_part('mailerlite', 'widget'); ?>
             </div>
-
-            <?php else: ?>
-
+          <?php else : ?>
             &nbsp;
-
-            <?php endif;?>
-
-
+          <?php endif; ?>
         </article>
-        <?php endwhile; else: ?>
-
-        <article>
-            <p>No hay contenido a mostrar</p>
-        </article>
-
-        <?php endif; ?>
-    </div>
+      <?php endwhile;
+    else : ?>
+      <article>
+        <p>No hay contenido a mostrar</p>
+      </article>
+    <?php endif; ?>
+  </div>
 </main>
 
 <?php get_footer(); ?>
