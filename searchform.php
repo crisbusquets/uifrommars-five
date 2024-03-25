8c5edd7c47a3574228ee2920bb78d6c8acb2274e
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2024-03-25 15:54:03 +0100

    Format searchform.php

--- searchform.php
+++ searchform.php
@@ -1,12 +1,9 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-    <label>
-        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
-        <input type="search" id="search-input" class="search-field"
-            placeholder="<?php echo esc_attr_x( 'Palabra clave', 'placeholder' ) ?>"
-            value="<?php echo get_search_query() ?>" name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-    </label>
-    <?php wp_dropdown_categories('show_option_none=Cualquier categoría'); ?>
-    <input type="hidden" name="post_type" value="post" />
-    <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Buscar', 'submit button' ) ?>" />
+<form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+  <label>
+    <span class="screen-reader-text"><?php echo _x('Search for:', 'label') ?></span>
+    <input type="search" id="search-input" class="search-field" placeholder="<?php echo esc_attr_x('Palabra clave', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
+  </label>
+  <?php wp_dropdown_categories('show_option_none=Cualquier categoría'); ?>
+  <input type="hidden" name="post_type" value="post" />
+  <input type="submit" class="search-submit" value="<?php echo esc_attr_x('Buscar', 'submit button') ?>" />
 </form>
