e61a8dcf2eca89545b95524ab4185990a72216e4
Cris Busquets <ileo.red@gmail.com>
2024-03-26 19:00:13 +0100

    Fix spacing issue on search results

--- search.php
+++ search.php
@@ -4,25 +4,26 @@
   <div class="wrapper">
     <div class="page-title">
       <h1>Resultados de búsqueda</h1>
-      <p>Existen <strong><?php echo $wp_query->found_posts; ?></strong><?php _e('artículos sobre', 'locale'); ?> <span class="search-highlight"><?php the_search_query(); ?></span>.</p>
+      <p>Existen <strong><?php echo $wp_query->found_posts; ?></strong> <?php _e('artículos sobre', 'locale'); ?> <span
+          class="search-highlight"><?php the_search_query(); ?></span>.</p>
     </div>
     <section class="content-grid">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          <article id="post-<?php the_ID(); ?>" <?php post_class('search-results'); ?>>
-            <?php $title = get_the_title();
+      <article id="post-<?php the_ID(); ?>" <?php post_class('search-results'); ?>>
+        <?php $title = get_the_title();
             $keys = explode(" ", $s);
             $title = preg_replace('/(' . implode('|', $keys) . ')/iu', '<span class="search-highlight">\0</span>', $title); ?>
-            <a href="<?php the_permalink(); ?>" class="search-image">
-              <?php the_post_thumbnail('blog-thumbnails'); ?>
-            </a>
-            <div class="category-link-list"><?php the_category(' ') ?></div>
-            <h3>
-              <a href="<?php the_permalink(); ?>">
-                <?php echo $title; ?>
-              </a>
-            </h3>
-          </article>
-        <?php endwhile;  ?>
+        <a href="<?php the_permalink(); ?>" class="search-image">
+          <?php the_post_thumbnail('blog-thumbnails'); ?>
+        </a>
+        <div class="category-link-list"><?php the_category(' ') ?></div>
+        <h3>
+          <a href="<?php the_permalink(); ?>">
+            <?php echo $title; ?>
+          </a>
+        </h3>
+      </article>
+      <?php endwhile;  ?>
     </section>
     <nav class="pagination">
       <?php echo paginate_links(array(
@@ -36,9 +37,9 @@
           'next_text'     => __('&raquo;'),
         )); ?>
     </nav>
-  <?php else : ?>
+    <?php else : ?>
     <p>No hay contenido a mostrar</p>
-  <?php endif; ?>
+    <?php endif; ?>
   </div>
 </main>
 
