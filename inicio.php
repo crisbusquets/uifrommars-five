5763ee81faa1a49d24f4d2a71f23ece21ccf1cf9
Cris Busquets <ileo.red@gmail.com>
2024-03-29 16:06:44 +0100

    Fix article spacing

--- inicio.php
+++ inicio.php
@@ -10,7 +10,8 @@
         <?php get_template_part('mailerlite', 'widget'); ?>
       </div>
       <div class="lottie">
-        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/uifrommars-hero.svg" width="454" height="329" alt="Únete a uiFromMars" />
+        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/uifrommars-hero.svg" width="454" height="329"
+          alt="Únete a uiFromMars" />
       </div>
     </section>
     <section class="one-column">
@@ -21,17 +22,17 @@
         $last_posts = get_posts(array('posts_per_page' => 6));
         foreach ($last_posts as $post) :
           setup_postdata($post); ?>
-          <article>
-            <a href="<?php the_permalink(); ?>" aria-label="Lee más sobre <?php the_title(); ?>" class="category-image">
-              <?php the_post_thumbnail('blog-thumbnails'); ?>
+        <article class="home-item">
+          <a href="<?php the_permalink(); ?>" aria-label="Lee más sobre <?php the_title(); ?>" class="category-image">
+            <?php the_post_thumbnail('blog-thumbnails'); ?>
+          </a>
+          <div class="category-link-list"><?php the_category(' ') ?></div>
+          <h3>
+            <a href="<?php the_permalink(); ?>">
+              <?php the_title(); ?>
             </a>
-            <div class="category-link-list"><?php the_category(' ') ?></div>
-            <h3>
-              <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-              </a>
-            </h3>
-          </article>
+          </h3>
+        </article>
         <?php endforeach;
         wp_reset_postdata(); ?>
       </div>
@@ -43,19 +44,19 @@
         <?php
         $post_id = array(1754, 1241, 1299, 2368, 1382, 592, 3442, 4008, 121, 3667);
         foreach ($post_id as $post) : ?>
-          <article class="most-read-grid">
-            <div class="most-category">
-              <div class="category-link-list"><?php the_category(' ') ?></div>
-            </div>
-            <div class="most-title">
-              <h3>
-                <a href="<?php the_permalink(); ?>" class="<?php echo ('gtm_web_most-read-' . $counter);
+        <article class="most-read-grid">
+          <div class="most-category">
+            <div class="category-link-list"><?php the_category(' ') ?></div>
+          </div>
+          <div class="most-title">
+            <h3>
+              <a href="<?php the_permalink(); ?>" class="<?php echo ('gtm_web_most-read-' . $counter);
                                                             $counter++; ?>">
-                  <?php the_title(); ?>
-                </a>
-              </h3>
-            </div>
-          </article>
+                <?php the_title(); ?>
+              </a>
+            </h3>
+          </div>
+        </article>
         <?php endforeach;
         wp_reset_postdata(); ?>
       </div>
