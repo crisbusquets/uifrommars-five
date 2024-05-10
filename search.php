9f0451388a468407e9cdff442f7337a24d86b976
Cris Busquets <ileo.red@gmail.com>
2024-05-10 08:58:50 +0200

    Fix search results h3

--- search.php
+++ search.php
@@ -17,7 +17,7 @@
           <?php the_post_thumbnail('blog-thumbnails'); ?>
         </a>
         <span class="pill"><?php the_category(' ') ?></span>
-        <h3>
+        <h3 class="post-title-home">
           <a href="<?php the_permalink(); ?>">
             <?php echo $title; ?>
           </a>
