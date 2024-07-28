a72d097ac164b25ac4018ccdd6dcd53e5d257a26
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2024-07-28 19:27:39 +0200

    Revert "Add Ko-fi widget"
    This reverts commit 356fe62776ce54828f6722fa2eb5a9e37e055f8f.

--- footer.php
+++ footer.php
@@ -1,14 +1,5 @@
 <?php wp_footer(); ?>
 
-<a href="https://ko-fi.com/uifrommars" class="plausible-event-name=Ko-fi+Widget" target="_blank"
-  style="text-decoration: none; color: inherit;">
-  <div id="kofi">
-    <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/ko-fi_logo-min.png"
-      style="width:40px; height: 40px;" alt="Ko-fi de uiFromMars">
-    ¿Un café?
-  </div>
-</a>
-
 <footer>
   <div class="wrapper">
     <div id="footer-container">
